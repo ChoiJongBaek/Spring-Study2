05e37acf038961c970531a2bbffaf8d248c127dd
ChoiJongBaek <whdqor94@naver.com>
2023-03-04 16:08:06 +0900

    2023.03.04 생성자 주입의 특징

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -9,6 +9,9 @@ import org.springframework.stereotype.Component;
 @Component
 public class OrderServiceImpl implements OrderService {
 
+    // 생성자 주입을 사용할 경우 final 키워드를 사용할 수 있다.
+    // final 키워드의 장점 : 생성할 때 정해지면 바뀌지가 않는다.
+    // final 키워드를 주면 초기에 값을 생성하거나 생성자를 통해서만 값을 넣어줄 수 있다.
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
 
