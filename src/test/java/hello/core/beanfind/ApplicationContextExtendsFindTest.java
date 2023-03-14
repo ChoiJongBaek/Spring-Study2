d66df305de76bf0f1023eb80b3844542d68a64f8
ChoiJongBaek <whdqor94@naver.com>
2023-03-14 22:36:19 +0900

    2023.03.14 프로토타입 스코프

--- src/test/java/hello/core/beanfind/ApplicationContextExtendsFindTest.java
+++ src/test/java/hello/core/beanfind/ApplicationContextExtendsFindTest.java
@@ -30,6 +30,7 @@ public class ApplicationContextExtendsFindTest {
     @DisplayName("부모 타입으로 조회 시, 자식이 둘 이상 있으면, 빈 이름을 지정하면 된다.")
     void findBeanByParentTypeBeanName() {
         DiscountPolicy rateDiscountPolicy = ac.getBean("rateDiscountPolicy", DiscountPolicy.class);
+        System.out.println("rateDiscountPolicy = " + rateDiscountPolicy);
         assertThat(rateDiscountPolicy).isInstanceOf(DiscountPolicy.class);
     }
 
@@ -37,6 +38,7 @@ public class ApplicationContextExtendsFindTest {
     @DisplayName("특정 하위 타입으로 조회")
     void findBeanBySubType() {
         RateDiscountPolicy bean = ac.getBean(RateDiscountPolicy.class);
+        System.out.println("bean = " + bean);
         assertThat(bean).isInstanceOf(RateDiscountPolicy.class);
     }
 
